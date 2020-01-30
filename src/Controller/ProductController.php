0e7bd2b86d184d63acf91288cf4c04be3b3e5ab7
Nicolas Rousseaux <rousseaux.nicolas@outlook.com>
2020-01-30 16:25:11 +0100

    add delete CSRF

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -37,4 +38,89 @@ class ProductController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+    * @Route("/product/all", name="product_list")
+    */
+
+    public function list()
+    {
+        // on recupére le depot qui contient nos produit
+        $productRepository = $this->getDoctrine()->getRepository(Product::class);
+        // SELECT * FROM product
+        $product = $productRepository->findAll();
+
+        if (!$product) {
+        	throw $this->createNotFoundException('Il n\'y a pas de produit enregistré');
+        }
+
+        return $this->render('product/list.html.twig', [
+            'products' => $product,
+        ]);
+    }
+
+    /**
+    * @Route("/product/udapte/{id}", name="product_udapte")
+    */
+    public function udapte(Request $request, Product $product)
+    {
+        // on crée le formulaire
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('product_list');
+
+            $this->addFlash('success', 'Le produit a bien été modifier.');
+
+        }
+
+        return $this->render('product/udapte.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/product/delete/{id}", name="product_deleted", methods={"POST"})
+     */
+    public function deleted(Request $request, Product $product, EntityManagerInterface $entityManager)
+    {
+        // on verifie la validité du token CSRF On se protége des faille CSRF
+        if ($this->isCsrfTokenValid('delete', $request->get('token'))) {
+        $entityManager->remove($product);
+        $entityManager->flush();
+        }
+
+        $this->addFlash('success', 'Le produit a bien été supprimer.');
+
+        return $this->redirectToRoute('product_list');
+    }
+
+
+    /**
+    * @Route("/product/{id}", name="product_show")
+    */
+
+    public function show($id)
+    {
+        dump($id);
+        // on recupére le depot qui contient nos produit
+        $productRepository = $this->getDoctrine()->getRepository(Product::class);
+        // SELECT * FROM product WHERE id = $id
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+        	throw $this->createNotFoundException('Le produit ' . $id . ' n\'existe pas.');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+    
 }
