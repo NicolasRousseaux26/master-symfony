d9152d96475ad4dffd65bff4f9e8eb022b458114
Nicolas Rousseaux <rousseaux.nicolas@outlook.com>
2020-01-30 10:27:32 +0100

    creat navbar/requete db

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage()
+    {
+        $product = new Product();
+        $product->setName("iPhone");
+        $product->setDescription("Mon produit");
+        $product->setPrice(999);
+        $entityManager = $this->getDoctrine()->getManager();
+        //persist est le fait d'inserer/modiffier dans la pabase
+        $entityManager->persist($product);
+        // flush execute la requete
+        $entityManager->flush();
+
+        return $this->render('index/homepage.html.twig');
+    }
+}
