9e741b8ce3b414d101c4432b365bd683f81f5894
Nicolas Rousseaux <rousseaux.nicolas@outlook.com>
2020-02-04 14:39:34 +0100

    DataTransformer for tags

---
+++ src/Form/Type/TagsInputType.php
@@ -0,0 +1,42 @@
+<?php 
+
+namespace App\Form\Type;
+
+use App\Form\DataTransformer\TagsArrayToStringTransformer;
+use App\Repository\TagRepository;
+use Symfony\Bridge\Doctrine\Form\DataTransformer\CollectionToArrayTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class TagsInputType extends AbstractType
+{
+     /**
+     * @var TagRepository
+     */
+    private $tagRepository;
+
+    public function __construct(TagRepository $tagRepository)
+    {
+        $this->tagRepository = $tagRepository;
+    }
+
+    public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options)
+    {        
+
+        $builder
+            //transforme une collection doctrine en tableau
+            ->addModelTransformer(new CollectionToArrayTransformer(), true)
+
+            //transforme un tableau de tags en chaine de caractéres
+            ->addModelTransformer(
+                new TagsArrayToStringTransformer($this->tagRepository),
+                true
+        );
+    }
+
+    public function getParent()
+    {
+        return TextType::class;
+    }
+}
